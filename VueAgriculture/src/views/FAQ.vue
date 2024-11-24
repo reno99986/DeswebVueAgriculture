593ed6008f98fbbb7a0a55ea7a26990960d3dc6e
reno99986 <jamrock99986@gmail.com>
2024-11-24 15:44:07 +0800

    implement fix footer to my own page

--- VueAgriculture/src/views/FAQ.vue
+++ VueAgriculture/src/views/FAQ.vue
@@ -1,9 +1,27 @@
 <script setup>
 import HeroQuestion from "../components/pages/FAQ/HeroQuestion.vue";
 import Question from "../components/pages/FAQ/Question.vue";
+import { onMounted, onUpdated } from "vue";
+const checkFooterHeight = () => {
+  if (document.documentElement.scrollHeight <= window.innerHeight + 5) {
+    document.querySelector("footer").classList.add("fixed", "bottom-0");
+  } else {
+    document.querySelector("footer").classList.remove("fixed", "bottom-0");
+  }
+};
+
+onMounted(() => {
+  checkFooterHeight();
+  window.addEventListener("resize", checkFooterHeight);
+});
+
+onUpdated(() => {
+  checkFooterHeight();
+});
 </script>
 
 <template>
   <HeroQuestion />
   <Question />
+  <div class="spacer"></div>
 </template>
