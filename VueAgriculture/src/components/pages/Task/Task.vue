e9b9287b03f8f50c3bf7df36d6256062be775fbe
NikWasHere <11221039@student.itk.ac.id>
2024-12-09 12:30:38 +0800

    fix
    fix

--- VueAgriculture/src/components/pages/Task/Task.vue
+++ VueAgriculture/src/components/pages/Task/Task.vue
@@ -1,101 +1,45 @@
 <template>
-    <div class="profile-page">
-      <div class="team-container">
-        <ProfileCard
-          v-for="member in teamMembers"
-          :key="member.id"
-          :member="member"
-          @show-details="openModal"
-        />
-      </div>
-  
-      <Modal
-        :isVisible="isModalOpen"
-        :member="currentMember"
-        @close-modal="closeModal"
-      />
+    <div class="card">
+      <img :src="member.photo" alt="Profile Picture" />
+      <h3>{{ member.name }}</h3>
+      <p>{{ member.role }}</p>
+      <p>{{ member.technologies }}</p>
+      <button @click="showDetails">Detail</button>
     </div>
   </template>
   
   <script>
-//   import ProfileCard from '';
-  import Modal from '../Task/taskmodal.vue';
-  
   export default {
-    components: {
-      ProfileCard,
-      Modal
-    },
-    data() {
-      return {
-        teamMembers: [
-          {
-            id: 1,
-            photo: 'photo1.jpg',
-            name: 'Hairul Irawan',
-            role: 'Ketua Kelompok',
-            technologies: 'Vue, HTML, CSS',
-            description: 'PM & Home Page',
-            github: 'https://github.com/'
-          },
-          {
-            id: 2,
-            photo: 'photo2.jpg',
-            name: 'Niko Afandi Saputro',
-            role: 'Anggota Kelompok',
-            technologies: 'Vue, HTML, CSS',
-            description: 'Hasil Tangkapan Page',
-            github: 'https://github.com/NikWasHere'
-          },
-          {
-            id: 3,
-            photo: 'photo3.jpg',
-            name: 'Reno Fahrezi Purnomo',
-            role: 'Anggota Kelompok',
-            technologies: 'Vue, HTML, CSS',
-            description: 'FAQ Page',
-            github: 'https://github.com/'
-          },
-          {
-            id: 4,
-            photo: 'photo4.jpg',
-            name: 'Haikal',
-            role: 'Anggota Kelompok',
-            technologies: 'Vue, HTML, CSS',
-            description: 'Our Service Page',
-            github: 'https://github.com/'
-          },
-        ],
-        isModalOpen: false,
-        currentMember: {}
-      };
+    props: {
+      member: Object
     },
     methods: {
-      openModal(member) {
-        this.currentMember = member;
-        this.isModalOpen = true;
-      },
-      closeModal() {
-        this.isModalOpen = false;
+      showDetails() {
+        this.$emit('show-details', this.member); 
       }
     }
   }
   </script>
   
   <style scoped>
-  .profile-page {
-    padding: 20px;
-  }
-  .team-container {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 20px;
+  .card {
+    border: 1px solid #ddd;
+    padding: 16px;
+    text-align: center;
   }
   
-  @media (max-width: 768px) {
-    .team-container {
-      grid-template-columns: 1fr;
-    }
+  .card img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+  
+  button {
+    margin-top: 10px;
+    background-color: #2232df;
+    color: white;
+    border: none;
+    padding: 10px;
+    cursor: pointer;
   }
   </style>
-  
--- VueAgriculture/src/components/pages/Task/taskmodal.vue
+++ VueAgriculture/src/components/pages/Task/taskmodal.vue
@@ -1,46 +1,45 @@
 <template>
-    <div v-if="isVisible" class="modal-overlay">
-      <div class="modal-content">
-        <h3>{{ member.name }}</h3>
-        <p>{{ member.description }}</p>
-        <a :href="member.github" class="text-secondary">GitHub</a>
-        <p><button @click="closeModal" class="mt-4 bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700">Close</button></p>
-      </div>
+  <div v-if="isVisible" class="modal-overlay">
+    <div class="modal-content">
+      <h3>{{ member.name }}</h3>
+      <p>{{ member.description }}</p>
+      <a :href="member.github" class="text-secondary">GitHub</a>
+      <p><button @click="closeModal" class="mt-4 bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700">Close</button></p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      isVisible: Boolean,
-      member: Object
-    },
-    methods: {
-      closeModal() {
-        this.$emit('close-modal'); 
-      }
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isVisible: Boolean,
+    member: Object
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close-modal'); 
     }
   }
-  </script>
-  
-  <style scoped>
-  .modal-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .modal-content {
-    background-color: white;
-    padding: 20px;
-    border-radius: 5px;
-    width: 300px;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  width: 300px;
+}
+</style>
